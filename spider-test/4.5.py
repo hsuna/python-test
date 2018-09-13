843625e89e57f9f70cca2f57152d3f62115b7f80
hsuna <me@hsuna.com>
2018-09-14 01:17:47 +0800

    update

--- spider-test/4.5.py
+++ spider-test/4.5.py
@@ -5,39 +5,7 @@ from urllib3.exceptions import NewConnectionError
 import os  #导入os模块
 import time
 import json
-
-class ModelData():
-    def __init__(self):
-        self.headers = {
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Connection': 'keep-alive',
-            'Cookie': 'PHPSESSID=8p4rn22lhr5ukc49mta0qhqrp1; _ym_uid=1536831056995187252; _ym_d=1536831056; _ym_isad=2; _ym_visorc_30860931=w',
-            'Host': 'parts.kctparts.com',
-            'Referer': 'http://parts.kctparts.com/',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
-            'X-Requested-With': 'XMLHttpRequest',
-        }
-        self.web_url = 'http://parts.kctparts.com/'
-    def get_data(self, id, accept="*/*"):
-        headers = self.headers
-        headers['Accept'] = accept
-
-        try:
-            response = requests.get(self.web_url, params={
-                "r": "catalog/getModel",
-                "node_id": id
-            }, headers=headers)
-            print('读取数据：'+response.url)
-
-            if response.status_code == 200:
-                return response.text
-        
-        except Exception as e:
-            print('超时')
-
-        return None
-
+import re
 
 class KctpartsData():
     def __init__(self):  #类的初始化操作
@@ -53,60 +21,108 @@ class KctpartsData():
         driver.get(self.web_url)
         #self.scroll_down(driver=driver, times=5)  #执行网页下拉到底部操作，执行5次
         print('开始获取tree')
-
-        all_a = BeautifulSoup(driver.page_source, 'lxml').select('a[href^="/#!hyundai-model"]')
         
-        #response = requests.get(self.web_url, headers=self.headers)
-        #all_a = BeautifulSoup(response.text, 'lxml').select('a[href^="/#!hyundai-model"]')
+        all_a = BeautifulSoup(driver.page_source, 'lxml').select('a[href^="/#!hyundai-model"]')
 
-        tree = []
+        name_rule = re.compile(r'[\/\\*:?<>"|]*')
         
         for a in all_a:
-            li_3 = a.find_parent('li')
+            li_3 = a.find_parent('li').find_parent('li')
             li_2 = li_3.find_parent('li')
             li_1 = li_2.find_parent('li')
 
             id = a['href'].replace("/#!", "")
 
-            lv_1 = li_1.span.get_text(),
-            lv_2 = li_2.span.get_text(),
-            lv_3 = li_3.span.get_text(),
-            lv_4 = a.get_text()
-            
-            model = ModelData()
-            data = model.get_data(id, "application/json, text/javascript, */*; q=0.01")
+            lv_1 = li_1.span.get_text().replace(' ', "_")
+            lv_2 = li_2.span.get_text().replace(' ', "_")
+            lv_3 = li_3.span.get_text().replace(' ', "_")
+            lv_4 = a.get_text().replace(' ', "_")
+            data = self.request(id, "catalog/getModel", "application/json, text/javascript, */*; q=0.01")
 
             if data:
-                data = json.loads(data)
+                data = json.loads(data.text)
                 for key,values in data.items():
-                    lv_5 = values["text"]
+                    lv_5 = values["text"].replace(' ', "_")
+                    print(self.folder_path, lv_1, lv_2, lv_3, lv_4, lv_5)
+                    path = os.path.join(self.folder_path, lv_1, lv_2, lv_3, lv_4, lv_5)
+                    self.mkdir(path)
+                    os.chdir(path) #切换文件夹
 
                     for child in values["children"]:
                         spanSoup = BeautifulSoup(child["text"], 'lxml')
-                        lv_6 = spanSoup.a.get_text()
+                        name = spanSoup.a.get_text().replace(' ', "_")
+                        name = re.sub(name_rule, '', name)+'.json'
 
                         id = spanSoup.span["id"]
-                        subas = ModelData()
-                        data = subas.get_data(id)
-                        if data:
-                            #tableSoup = BeautifulSoup(data, 'lxml').select('#items_list table')
+                        self.save_file(id, os.path.join(name))
 
-                            tree.append({
-                                'lv_1': lv_1,
-                                'lv_2': lv_2,
-                                'lv_3': lv_3,
-                                'lv_4': lv_4,
-                                'lv_5': lv_5,
-                                'lv_6': lv_6,
-                                'table': data,
-                            }) 
-                    
-        return tree
+    def save_file(self, id, file_name):
+        file = self.request(id, 'catalog/getSpares')
+        if file:
+            print('开始解析文件')
+            content = self.analy_file(file)
+            print('开始保存文件数据')
+            f = open(file_name, 'a+')
+            f.write(content)
+            print(file_name, '文件保存成功！')
+            f.close()
+
+    def analy_file(self, file):
+        items = []
+        table = BeautifulSoup(file.text, 'lxml').find(id='items_list').find('table')
+        ths = table.select('thead th')
+        trs = table.select('tbody tr')
+
+        for tr in trs:
+            tds = tr.find_all('td')
+            tmp = {}
+            for i, th in enumerate(ths):
+                if th.get_text().replace('\u00a0', "") != "":
+                    tmp[th.get_text()] = tds[i].get_text()
+
+            items.append(tmp)
+
+        return json.dumps(items)
+
+    def request(self, node_id, r, accept="*/*"):
+        headers = {
+            'Accept': accept,
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Cookie': 'PHPSESSID=8p4rn22lhr5ukc49mta0qhqrp1; _ym_uid=1536831056995187252; _ym_d=1536831056; _ym_isad=2; _ym_visorc_30860931=w',
+            'Host': 'parts.kctparts.com',
+            'Referer': 'http://parts.kctparts.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+
+        try:
+            r = requests.get('http://parts.kctparts.com/', params={
+                "r": r,
+                "node_id": node_id
+            }, headers=headers)
+            print('开始请求文件数据：', node_id)
+
+            if r.status_code == 200:
+                return r
+        
+        except Exception as e:
+            print('请求文件失败：', node_id)
+
+        return False
+
+    def mkdir(self, path):  ##这个函数创建文件夹
+        path = path.strip()
+        isExists = os.path.exists(path)
+        if not isExists:
+            os.makedirs(path)
+            print('创建', path, '的文件夹')
+            return True
+        else:
+            return False
+
+
 
 kctparts = KctpartsData()  #创建类的实例
-tree = kctparts.get_data()  #执行类中的方法
-
-f = open('test1.json','w')
-f.write(json.dumps(tree))
-f.close()
-
+kctparts.get_data()  #执行类中的方法
