9da94269494554f78e958ac61bb369521c990806
hsuna <me@hsuna.com>
2018-09-16 12:30:27 +0800

    update

--- production/kctparts.py
+++ production/kctparts.py
@@ -28,6 +28,7 @@ class KctpartsData():
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'}  #给请求指定一个请求头来模拟chrome浏览器
         self.web_url = 'http://parts.kctparts.com/'  #要访问的网页地址
         self.folder_path = 'D:\Desktop\KctpartsData\data'  #设置存放的文件目录
+        self.cache_path = 'D:\Desktop\KctpartsData\cache'  #设置缓存的文件目录
         self.log_path = 'D:\Desktop\KctpartsData\log'  #设置存放的文件目录
         self.access_path = '' #历史记录
         self.error_path = '' #错误记录
@@ -37,7 +38,7 @@ class KctpartsData():
     def working(self):
         while True:
             arguments = self.q.get()
-            self.save_task(arguments)
+            self.queue_task(arguments)
             self.q.task_done()
 
     def get_data(self):
@@ -55,13 +56,15 @@ class KctpartsData():
         
         #创建主文件夹
         self.mkdir(self.folder_path)
+        self.mkdir(self.cache_path)        
         self.mkdir(self.log_path)
         #添加历史记录路径
         time_str = time.strftime("%Y%m%d%H%M%S", time.localtime())
         self.access_path = os.path.join(self.log_path, 'access_'+time_str+'.log')
         self.error_path = os.path.join(self.log_path, 'error_'+time_str+'.log')
 
-        all_a = BeautifulSoup(driver.page_source, 'lxml').select('a[href^="/#!hyundai-model"], a[href^="/#!doosan-model"], a[href^="/#!daemo-model"]')
+        all_a = BeautifulSoup(driver.page_source, 'lxml').select('.folder > a[href^="/#!zf"]')
+        #all_a = BeautifulSoup(driver.page_source, 'lxml').select('.folder > a[href^="/#!"]')
 
         name_rule = re.compile(r'[\/\\*:?<>"|]*')
         
@@ -82,55 +85,111 @@ class KctpartsData():
             while hasattr(li, 'span'):
                 dirs.insert(0, li.span.get_text())
                 li = li.find_parent('li')
-                
-            id = a['href'].replace("/#!", "")
-            data = self.request(id, "catalog/getModel", "application/json, text/javascript, */*; q=0.01")
-
-            if data:
-                data = json.loads(data.text)
-                if isinstance(data, dict):
-                    for key,values in data.items():
-                        paths = dirs.copy()
-                        paths.append(values["text"])
-
-                        for child in values["children"]:
-                            spanSoup = BeautifulSoup(child["text"], 'lxml')
-                            title = spanSoup.a.get_text()
-
-                            id = spanSoup.span["id"]
-                            file_name = id+'.json'
-                            file_path = os.path.join(self.folder_path, file_name) 
-                            isExists = os.path.exists(file_path)
-                            if not isExists:
-                                self.q.put({
-                                    "id":id, 
-                                    "title":title,
-                                    "file_name":file_name, 
-                                    "file_path":file_path,
-                                    "paths": paths
-                                })
-                            else:
-                                print('文件已经存在：', file_path)
-            else:
-                self.error('请求文件失败：', id)
+            
+            self.q.put({
+                "type": "list",
+                "href":a['href'],
+                "dirs": dirs
+            })
         
         #等待进程结束
         self.q.join()
 
-    def save_task(self, data):
-        file = self.request(data["id"], 'catalog/getSpares')
-        file_path = data["file_path"]
-        if file:
-            print('开始解析文件')
-            content = self.analy_file(data, file)
-            print('开始保存文件数据')
-            f = open(file_path, 'a+')
-            f.write(content)
-            self.log('文件保存成功：', file_path)
-            f.close()
+    def get_model_list(self, href, dirs):
+        id = re.match(r'/#!(.*/)?([^/]*)', href).group(2)
+        file_name = id.replace(":", "_")+'.json'
+        file_path = os.path.join(self.cache_path, file_name)
+        isExists = os.path.exists(file_path)
+        if not isExists:
+            data = self.request(id, "catalog/getModel", "application/json, text/javascript, */*; q=0.01")
+            if data:
+                content = data.text
+                self.save_file(file_path, content)
         else:
-            self.error('请求文件失败：', file_path)
+            content = self.get_file(file_path)
 
+
+        if content:
+            data = json.loads(content)
+            if isinstance(data, dict):
+                for key,values in data.items():
+                    t_dirs = dirs.copy()
+                    t_dirs.append(values["text"])
+                    self.get_model(values["children"], t_dirs)
+                    
+            elif isinstance(data, list):
+                self.get_model(data, dirs)
+
+        else:
+            self.error('请求文件失败：', id)
+
+    def get_model(self, children, dirs):
+        for child in children:
+            if "hasChildren" in child:
+                a = BeautifulSoup(child["text"], 'lxml').a
+                self.q.put({
+                    "type": "list",
+                    "href":a['href'],
+                    "dirs": dirs
+                })
+            else:
+                spanSoup = BeautifulSoup(child["text"], 'lxml')
+                title = spanSoup.a.get_text()
+
+                id = spanSoup.span["id"]
+                file_name = id.replace(":", "_")+'.json'
+                file_path = os.path.join(self.folder_path, file_name) 
+                isExists = os.path.exists(file_path)
+                if not isExists:
+                    self.q.put({
+                        "type": "file",
+                        "id":id, 
+                        "title":title,
+                        "file_name":file_name, 
+                        "file_path":file_path,
+                        "dirs": dirs
+                    })
+                else:
+                    print('文件已经存在：', file_path)
+
+    def get_file(self, filepath):
+        try:
+            f = open(filepath)
+            content = f.read()
+            f.close()
+            #print('读取文件数据：', filepath)
+            return content
+        except Exception as e:
+            return False
+        finally:
+            f.close()
+
+    def save_file(self, filepath, content):
+        try:
+            print('开始保存文件数据')
+            f = open(filepath, 'a+')
+            f.write(content)
+            self.log('文件保存成功：', filepath)
+            f.close()
+            return True
+        except Exception as e:
+            return False
+        finally:
+            f.close()
+
+    def queue_task(self, data):
+        if data["type"] == "file":
+            file = self.request(data["id"], 'catalog/getSpares')
+            file_path = data["file_path"]
+            if file:
+                print('开始解析文件')
+                content = self.analy_spares(data, file)
+                self.save_file(file_path, content)
+            else:
+                self.error('请求文件失败：', file_path)
+
+        elif data["type"] == "list":
+            self.get_model_list(data["href"], data["dirs"])
 
     def log(self, *content):
         print(*content)
@@ -144,7 +203,7 @@ class KctpartsData():
         f.write("  ".join(content)+'\n')
         f.close()
 
-    def analy_file(self, data, file):
+    def analy_spares(self, data, file):
         items = []
         table = BeautifulSoup(file.text, 'lxml').find(id='items_list').find('table')
         ths = table.select('thead th')
@@ -162,7 +221,7 @@ class KctpartsData():
             
         return json.dumps({
             'title': data["title"],
-            'dirs': data["paths"],
+            'dirs': data["dirs"],
             'data': items
         })
 
