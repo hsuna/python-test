04bfd382146842e5904b1cca646dc8dc1bfae86b
hsuna <me@hsuna.com>
2018-11-15 21:02:00 +0800

    update

--- scrapy/SecHouse/SecHouse/pipelines.py
+++ scrapy/SecHouse/SecHouse/pipelines.py
@@ -22,26 +22,45 @@ class MongoPipeline(object):
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
-        new_item = [{
-            'district'      : item['district'      ],
-            'mode'          : item['mode'          ],
-            'price'         : item['price'         ],
-            'location'      : item['location'      ],
-            'area'          : item['area'          ],
-            'downpayment'   : item['downpayment'   ],
-            'built'         : item['built'         ],
-            'orientation'   : item['orientation'   ],
-            'payment'       : item['payment'       ],
-            'house_type'    : item['house_type'    ],
-            'floor'         : item['floor'         ],
-            'decorate'      : item['decorate'      ],
-            'age'           : item['age'           ],
-            'elevator'      : item['elevator'      ],
-            'agelimit'      : item['agelimit'      ],
-            'property_right': item['property_right'],
-            'only'          : item['only'          ],
+        #house_item = [{
+        #    'district'      : item['district'      ],
+        #    'mode'          : item['mode'          ],
+        #    'price'         : item['price'         ],
+        #    'location'      : item['location'      ],
+        #    'area'          : item['area'          ],
+        #    'downpayment'   : item['downpayment'   ],
+        #    'built'         : item['built'         ],
+        #    'orientation'   : item['orientation'   ],
+        #    'payment'       : item['payment'       ],
+        #    'house_type'    : item['house_type'    ],
+        #    'floor'         : item['floor'         ],
+        #    'decorate'      : item['decorate'      ],
+        #    'age'           : item['age'           ],
+        #    'elevator'      : item['elevator'      ],
+        #    'agelimit'      : item['agelimit'      ],
+        #    'property_right': item['property_right'],
+        #    'only'          : item['only'          ],
+        #}]
 
+        mhouse_item = [{
+            'title'   :item['title'   ],
+            'tolprice':item['tolprice'],
+            'mode'    :item['mode'    ],
+            'area'    :item['area'    ],
+            'price'        :item['price'        ],
+            'orientation'  :item['orientation'  ],
+            'floor'        :item['floor'        ],
+            'decorate'     :item['decorate'     ],
+            'built'        :item['built'        ],
+            'house_type'   :item['house_type'   ],
+            'agelimit'     :item['agelimit'     ],
+            'elevator'     :item['elevator'     ],
+            'only'         :item['only'         ],
+            'budget'       :item['budget'       ],
+            'district'     :item['district'     ],
+            'traffic'      :item['traffic'      ],
         }]
-        self.collection.insert(new_item)
+
+        self.collection.insert(mhouse_item)
         logging.debug("Item wrote to MongoDB database %s/%s"%(settings['MONGODB_DB'],settings['MONGODB_COLLECTION']))
         return item
--- scrapy/SecHouse/SecHouse/spiders/spiders.py
+++ scrapy/SecHouse/SecHouse/spiders/spiders.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import scrapy
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from SecHouse.items import HouseItem
+from SecHouse.items import HouseItem,MHouseItem
 
 class HouseSpider(CrawlSpider):
     name = 'house'
     start_urls = ['https://guangzhou.anjuke.com/']
+    custom_settings = {
+        "DOWNLOAD_DELAY": 5,
+        "RANDOM_DELAY": 3
+    }
     rules = [
         Rule(LinkExtractor(allow=(r'https://guangzhou.anjuke.com/sale/p\d+'))),
         Rule(LinkExtractor(allow=(r'https://guangzhou.anjuke.com/prop/view/\w+')), callback="parse_item"),
@@ -21,16 +26,16 @@ class HouseSpider(CrawlSpider):
 
         # 存放房子信息
         item = HouseItem()
-        item['district'       ] = houseinfo[0 ].xpath('normalize-space(.//div[2]/a/text())').extract()[0]
-        item['mode'           ] = houseinfo[1 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
-        item['price'          ] = houseinfo[2 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
-        item['location'       ] = houseinfo[3 ].xpath('.//div[2]/p').xpath('normalize-space(string(.))').extract()[0]
-        item['area'           ] = houseinfo[4 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
-        item['downpayment'    ] = houseinfo[5 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
-        item['built'          ] = houseinfo[6 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
-        item['orientation'    ] = houseinfo[7 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
-        item['payment'        ] = houseinfo[8 ].xpath('normalize-space(.//div[2]/span/text())').extract()[0]
-        item['house_type'     ] = houseinfo[9 ].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['district'       ] = houseinfo[0].xpath('normalize-space(.//div[2]/a/text())').extract()[0]
+        item['mode'           ] = houseinfo[1].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['price'          ] = houseinfo[2].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['location'       ] = houseinfo[3].xpath('.//div[2]/p').xpath('normalize-space(string(.))').extract()[0]
+        item['area'           ] = houseinfo[4].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['downpayment'    ] = houseinfo[5].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['built'          ] = houseinfo[6].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['orientation'    ] = houseinfo[7].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        item['payment'        ] = houseinfo[8].xpath('normalize-space(.//div[2]/span/text())').extract()[0]
+        item['house_type'     ] = houseinfo[9].xpath('normalize-space(.//div[2]/text())').extract()[0]
         item['floor'          ] = houseinfo[10].xpath('normalize-space(.//div[2]/text())').extract()[0]
         item['decorate'       ] = houseinfo[11].xpath('normalize-space(.//div[2]/text())').extract()[0]
         item['age'            ] = houseinfo[12].xpath('normalize-space(.//div[2]/text())').extract()[0]
@@ -38,4 +43,54 @@ class HouseSpider(CrawlSpider):
         item['agelimit'       ] = houseinfo[14].xpath('normalize-space(.//div[2]/text())').extract()[0]
         item['property_right' ] = houseinfo[15].xpath('normalize-space(.//div[2]/text())').extract()[0]
         item['only'           ] = houseinfo[16].xpath('normalize-space(.//div[2]/text())').extract()[0]
+        return item
+
+
+class MobileHouseSpider(CrawlSpider):
+    name = 'mhouse'
+    start_urls = ['https://m.anjuke.com/gz/sale/']
+   
+    def start_requests(self):
+        for i in range(1, 500):
+            url = 'https://m.anjuke.com/gz/sale/?from=anjuke_home&page='+str(i)
+            yield scrapy.Request(url, callback=self.parse_page, meta={
+                'dont_redirect': True,
+                'handle_httpstatus_list': [302]
+            })
+
+    def parse_page(self, response):
+        selector = Selector(response)
+        urls = selector.xpath('//a[contains(@class, "house-item")]/@href').extract()
+        
+        for url in urls:
+            yield scrapy.Request(url, callback=self.parse_item, meta={
+                'dont_redirect': True,
+                'handle_httpstatus_list': [302]
+            })
+
+    def parse_item(self, response):
+        selector = Selector(response)
+
+        # 存放房子信息
+        item = MHouseItem()
+        
+        housebasic = selector.xpath('//div[@class="house-info-content"]')
+        item['title'          ] = housebasic.xpath('normalize-space(.//div[@class="house-address"]/text())').extract()[0]
+        item['tolprice'       ] = housebasic.xpath('normalize-space(.//div[@class="house-data"]/span[1]/text())').extract()[0]
+        item['mode'           ] = housebasic.xpath('normalize-space(.//div[@class="house-data"]/span[2]/text())').extract()[0]
+        item['area'           ] = housebasic.xpath('normalize-space(.//div[@class="house-data"]/span[3]/text())').extract()[0]
+
+        houseinfo = selector.xpath('//ul[@class="info-list"]/li')
+        item['price'          ] = houseinfo[0].xpath('normalize-space(./text())').extract()[0]
+        item['orientation'    ] = houseinfo[1].xpath('normalize-space(./text())').extract()[0]
+        item['floor'          ] = houseinfo[2].xpath('normalize-space(./text())').extract()[0]
+        item['decorate'       ] = houseinfo[3].xpath('normalize-space(./text())').extract()[0]
+        item['built'          ] = houseinfo[4].xpath('normalize-space(./text())').extract()[0]
+        item['house_type'     ] = houseinfo[5].xpath('normalize-space(./text())').extract()[0]
+        item['agelimit'       ] = houseinfo[6].xpath('normalize-space(./text())').extract()[0]
+        item['elevator'       ] = houseinfo[7].xpath('normalize-space(./text())').extract()[0]
+        item['only'           ] = houseinfo[8].xpath('normalize-space(./text())').extract()[0]
+        item['budget'         ] = houseinfo[9].xpath('normalize-space(./a/text())').extract()[0]
+        item['district'       ] = houseinfo[10].xpath('normalize-space(./a/text())').extract()[0] + houseinfo[10].xpath('normalize-space(./text())').extract()[0]
+        item['traffic'        ] = houseinfo[11].xpath('normalize-space(./text())').extract()[0]
         return item
