921969553084713b97ca52b911e353ff437fc0dc
hsuna <me@hsuna.com>
2018-11-16 19:47:24 +0800

    update

---
+++ scrapy/SecHouse/build/lib/SecHouse/items.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HouseItem(scrapy.Item):
+    # 所属小区
+    district = scrapy.Field()
+    # 房屋户型
+    mode = scrapy.Field()
+    # 房屋单价
+    price = scrapy.Field()
+    # 所在位置
+    location = scrapy.Field()
+    # 建筑面积
+    area = scrapy.Field()
+    # 参考首付
+    downpayment = scrapy.Field()
+    # 建造年代
+    built = scrapy.Field()
+    # 房屋朝向
+    orientation = scrapy.Field()
+    # 参考月供
+    payment = scrapy.Field()
+    # 房屋类型
+    house_type = scrapy.Field()
+    # 所在楼层
+    floor = scrapy.Field()
+    # 装修程度
+    decorate = scrapy.Field()
+    # 产权年限
+    age = scrapy.Field()
+    # 配套电梯
+    elevator = scrapy.Field()
+    # 房本年限
+    agelimit = scrapy.Field()
+    # 产权性质
+    property_right = scrapy.Field()
+    # 唯一住房
+    only = scrapy.Field()
+
+class MHouseItem(scrapy.Item):
+    # 标题
+    title = scrapy.Field()
+    # 房屋总价
+    tolprice = scrapy.Field()
+    # 房屋户型
+    mode = scrapy.Field()
+     # 建筑面积
+    area = scrapy.Field()
+
+    # 房屋单价
+    price = scrapy.Field()
+    # 房屋朝向
+    orientation = scrapy.Field()
+    # 所在楼层
+    floor = scrapy.Field()
+    # 装修程度
+    decorate = scrapy.Field()
+    # 建造年代
+    built = scrapy.Field()
+    # 房屋类型
+    house_type = scrapy.Field()
+    # 房本年限
+    agelimit = scrapy.Field()
+    # 配套电梯
+    elevator = scrapy.Field()
+    # 唯一住房
+    only = scrapy.Field()
+    # 参考月供
+    budget = scrapy.Field()
+    # 所属小区
+    district = scrapy.Field()
+    # 交通
+    traffic = scrapy.Field()
+
---
+++ scrapy/SecHouse/build/lib/SecHouse/settings.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for SecHouse project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'SecHouse'
+
+SPIDER_MODULES = ['SecHouse.spiders']
+NEWSPIDER_MODULE = 'SecHouse.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'SecHouse (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'SecHouse.middlewares.SechouseSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+#    'SecHouse.middlewares.ProxyMiddleware': 543,
+#    'SecHouse.middlewares.SechouseDownloaderMiddleware': 543,
+}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    'SecHouse.pipelines.MongoPipeline': 1,
+#    'SecHouse.pipelines.SechousePipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# MongoDB settings
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DB = 'python'
+MONGODB_COLLECTION = 'sechouse'
+
+# Proxys settings
+PROXIES = [
+    "http://47.106.96.233:80",
+    "http://117.191.11.72:80",
+    "http://39.105.95.204:80",
+    "http://61.176.223.7:58822",
+    "http://183.166.129.53:8080",
+    "http://61.183.176.122:57210",
+    "http://218.204.129.195:43809",
+]
