296d685efd638e7472b8ce5f350fc1805539f4ec
hsuna <me@hsuna.com>
2019-01-25 14:01:42 +0800

    花瓣爬虫

---
+++ scrapy/Huaban/Huaban/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HuabanItem(scrapy.Item):
+    # define the fields for your item here like:
+    # 目录名
+    imgDir = scrapy.Field()
+    # 类型
+    imgType = scrapy.Field()
+    # 图片名
+    imgName = scrapy.Field()
+    # 图片链接
+    imgUrl = scrapy.Field()
+    
